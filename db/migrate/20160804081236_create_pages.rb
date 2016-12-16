0c4960a97c22ef77245f31a3823badeced6dfbc1
Amit Patel <amit.patel@botreeconsulting.com>
2016-12-16 22:50:07 +0530

    Migrate to Rails 5

--- db/migrate/20160804081236_create_pages.rb
+++ db/migrate/20160804081236_create_pages.rb
@@ -2,7 +2,7 @@ class CreatePages < ActiveRecord::Migration
   def change
     create_table :pages do |t|
       t.string :url
-
+      t.index :url
       t.timestamps null: false
     end
   end
--- db/migrate/20160804081308_create_page_contents.rb
+++ db/migrate/20160804081308_create_page_contents.rb
@@ -1,7 +1,7 @@
 class CreatePageContents < ActiveRecord::Migration
   def change
     create_table :page_contents do |t|
-      t.references :page
+      t.references :page, foreign_key: true
       t.integer :tag_type
       t.text :tag_content
 
