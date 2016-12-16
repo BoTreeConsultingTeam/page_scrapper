98715116757eec40f0deaa76e8865e665c9ef93a
Amit Patel <amit.patel@botreeconsulting.com>
2016-12-16 23:31:00 +0530

    Commiting unsaved change.

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,4 +1,4 @@
-class Page < ActiveRecord::Base
+class Page < ApplicationRecord
   has_many :page_contents, dependent: :destroy
   validates :url, presence: true, uniqueness: {message: 'is already scrapped!'}
   accepts_nested_attributes_for :page_contents
